43f89a3d6ebff5ad1b0af41f3484244218df9edd
gmgn120 <gmgn120@gmail.com>
2020-12-05 03:55:09 +0000

    [add]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e7cabca740634138aefc7bb69943a76f406562fbbf11c04c0102d83c79a5fcf87ce512deb19db7bba80d2f1051e71c6d5adb50d9d04f6b965d406ae88b598701'
